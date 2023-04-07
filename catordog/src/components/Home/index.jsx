dc638fd10ee6919a6dd0f24b44278791e5142bce
Gabriel_FS13 <falcaoshadowbr@gmail.com>
2023-04-06 23:13:13 -0300

    correção de texto

--- catordog/src/components/Home/index.jsx
+++ catordog/src/components/Home/index.jsx
@@ -8,7 +8,7 @@ export default function Home(){
                     Seu objetivo aqui é simples, haverá uma imagem na tela a imagem de um animal,
                     você deve distinguir se é um gato ou um cachorro, caso seja um cachorro, arraste para direita,
                     caso seja um gato arraste para esquerda.
-                    Serão ao todo 30 imagens, no final, se apresentado um relatório dizendo o quão bom você é em diferenciar esses animais.
+                    Serão ao todo 30 imagens, no final, será apresentado um relatório dizendo o quão bom você é em diferenciar esses animais.
                     Boa sorte!
                 </p>
             </div>
